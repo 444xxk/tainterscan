c0e9109b80576149278997dece9adfdfe623db7b
He Wang <he.wang@doclerholding.com>
2017-03-24 10:43:18 +0100

    test

--- tainterscan.php
+++ tainterscan.php
@@ -24,7 +24,6 @@ try {
     echo 'Parse Error: ', $e->getMessage();
   };
 
-
 //echo(array_key_exists("_GET",$stmts));
 echo "Searching for tainted inputs by walking recursively the PHPParser array \n"; 
 
@@ -40,55 +39,49 @@ array_walk($stmts, 'is_tainted');
 function is_tainted($item, $key)
 {
 
-echo "[DEBUG] function is_tainted started on \n";
-var_dump($item);
-echo "DEBUG start taint scan \n"; 
+//echo "[DEBUG] function is_tainted started on \n";
+//var_dump($item);
+//echo "DEBUG start taint scan \n"; 
+//if ($item == "_GET"){ echo "detection without iteration !! TAINTED !!\n"; } 
 
-if ($item == "_GET"){ echo "detection without iteration !! TAINTED !!\n"; } 
-
-
-foreach ($item as $objectkey => $value) {
-	// 
-	echo "iterating the array for value scan ...\n"; 
+foreach ((array)$item as $objectkey => $value) {
+	//echo "iterating the array for value scan ...\n"; 
 	if ($value == "_GET"){
-
 // replace with all user inputs source 
 // we need to code a function for taint detection
 // need to get the full path of this input (Obj > x > y > z);
-
 	echo "IS TAINTED DETECTED !!! >>>> ";
-	    var_dump($value); 
-	    echo ("in the objet " . var_dump($item));
-            var_dump($item -> $objectkey); 
-	
-						}
+	var_dump($value); 
+	echo ("in the objet " . var_dump($item));
+        //var_dump($item -> $objectkey);
+        var_dump($key); 						}
 }; 
 
 
 if (is_object($item))
 {
-	echo "this is an object !\n";
-	echo "[DEBUG] recursion on object \n";	
+	//echo "this is an object !\n";
+	//echo "[DEBUG] recursion on object \n";	
 	array_walk($item,'is_tainted');
 };
 
 if (is_array($item))
 { 
 	array_walk_recursive($item,'is_tainted');
-	echo "[DEBUG] recursion on array \n";	    
+	//echo "[DEBUG] recursion on array \n";	    
 }; 
 	    
  
-  }; // end of function  
+}; // end of function  
 
 
 
 
-function test_print($item, $key)
-{
-    //echo "$key holds $item\n";
-    var_dump($item);
-}
+// function test_print($item, $key)
+// {
+//     //echo "$key holds $item\n";
+//     var_dump($item);
+// }
 
 
 function is_user_tainted()
