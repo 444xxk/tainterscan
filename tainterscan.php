b6303af0b418559ccc87775b370909c14ea0450b
wwwwash <he.wang@doclerholding.com>
2017-03-24 15:41:23 +0100

    fix a bit

--- tainterscan.php
+++ tainterscan.php
@@ -18,18 +18,16 @@ $nodeDumper = new PhpParser\NodeDumper;
 echo "[DEBUG] Pretty print Node dumper from PHPParser \n\n"; 
 try {
     $stmts = $parser->parse($code);
-    //var_dump($stmts);
-    echo $nodeDumper->dump($stmts), "\n";
+    var_dump($stmts);
+    //echo $nodeDumper->dump($stmts), "\n";
 } catch (PhpParser\Error $e) {
     echo 'Parse Error: ', $e->getMessage();
   };
 
 //echo(array_key_exists("_GET",$stmts));
-echo "Searching for tainted inputs by walking recursively the PHPParser array \n"; 
+echo "Searching for tainted inputs by walking recursively the PHPParser array \n\n"; 
 
 // SCAN FUNCTIOM HERE
-
-//array_walk_recursive($stmts, 'test_print');
 array_walk($stmts, 'is_tainted');
 
 
@@ -41,38 +39,46 @@ function is_tainted($item, $key)
 
 //echo "[DEBUG] function is_tainted started on \n";
 //var_dump($item);
-//echo "DEBUG start taint scan \n"; 
+echo "[DEBUG] start taint scan \n"; 
 //if ($item == "_GET"){ echo "detection without iteration !! TAINTED !!\n"; } 
 
-foreach ((array)$item as $objectkey => $value) {
+foreach ($item as $objectkey => $value) {
 	//echo "iterating the array for value scan ...\n"; 
 	if ($value == "_GET"){
 // replace with all user inputs source 
 // we need to code a function for taint detection
 // need to get the full path of this input (Obj > x > y > z);
-	echo "IS TAINTED DETECTED !!! >>>> ";
+	echo "***************** !!! IS TAINTED DETECTED !!! ***************** >>>> ";
 	var_dump($value); 
 	echo ("in the objet " . var_dump($item));
-        //var_dump($item -> $objectkey);
-        var_dump($key); 						}
-}; 
+        var_dump($item -> $objectkey); 
+        //var_dump($key); 						
+    }
 
+
+     
+}
 
 if (is_object($item))
 {
-	//echo "this is an object !\n";
-	//echo "[DEBUG] recursion on object \n";	
-	array_walk($item,'is_tainted');
+    //echo "this is an object !\n";
+    echo "[DEBUG] recursion on object \n"; 
+    var_dump($item); 
+    echo "\n"; echo"**************************";echo "\n";  
+    array_walk($item,'is_tainted');
 };
 
 if (is_array($item))
 { 
-	array_walk_recursive($item,'is_tainted');
-	//echo "[DEBUG] recursion on array \n";	    
-}; 
-	    
+    echo "[DEBUG] recursion on array \n";
+    var_dump($item);
+    echo "\n"; echo"**************************";echo "\n";   
+    array_walk_recursive($item,'is_tainted');
+         
+};
+   
  
-}; // end of function  
+} // end of function  
 
 
 
@@ -87,6 +93,4 @@ if (is_array($item))
 function is_user_tainted()
 {
 // $_ variables , load from config etc ...
-} 
-
-
+} ;
