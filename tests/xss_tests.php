3a8564b1e39844e46b136ab4eb21c76e53d4e1c4
darksh3llRU <trololo@lol.com>
2017-04-10 22:54:59 +0200

    xss tests

--- tests/xss_tests.php
+++ tests/xss_tests.php
@@ -3,9 +3,4 @@
 $name = $_REQUEST ['name'];
 ?>
 <html><body>Hello, <?php echo $name; ?>!</body></html>
-
-//XSS2
-<?php
-$name = $_REQUEST ['name'];
-?>
 <html><body>Hello, <?php echo "lol"; ?>!</body></html>
